7c846b19753a7e81f210344d22cbf938eba20ac6
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-08-21 17:49:27 +0200

    a tracker's first time

--- relearn/urls.py
+++ relearn/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,5 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^tracker/$', TemplateView.as_view(template_name="tracker.html")),
 )
 
