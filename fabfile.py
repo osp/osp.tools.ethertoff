ad41e156efef37f66b640ddeb107313c9a6c65b0
Eric Schrijver <eric@ericschrijver.nl>
2013-08-21 17:35:13 +0200

    Deploy script

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import run, cd, env, local
+
+
+env.hosts = ['osp@osp.constantvzw.org']
+env.path = '/home/osp/apps/relearn/relearn.be/'
+
+def getdb():
+    pass
+
+def deploy():
+    with cd(env.path):
+        run('git pull origin master')
+        run('/home/osp/apps/venvs/relearn/bin/python manage.py collectstatic --noinput')
+        sudo('supervisorctl restart relearn')
