6f78b5b0d8d01ac5398d4bd7f922fb9e1b039162
Alexandre Leray <alexandre@alexandreleray.com>
2013-08-25 22:03:50 +0200

    Updated the email bot so it render HTML using django templates

--- list-bot/imapbot.py
+++ list-bot/imapbot.py
@@ -4,15 +4,20 @@ read.
 """
 
 
+import os
 import email
 import imaplib
-import settings
+import settings as bot_settings
+
+from django.template import Template, Context
+from django.template import loader
+from django.conf import settings
 
 
-conn = imaplib.IMAP4_SSL(settings.SERVER)
+conn = imaplib.IMAP4_SSL(bot_settings.SERVER)
 
 try:
-    (retcode, capabilities) = conn.login(settings.USER, settings.PASSWORD)
+    (retcode, capabilities) = conn.login(bot_settings.USER, bot_settings.PASSWORD)
 except:
     print sys.exc_info()[1]
     sys.exit(1)
@@ -22,22 +27,33 @@ ret = conn.select() # Select inbox or default namespace
 (retcode, messages) = conn.search(None, '(UNSEEN)')
 
 if retcode == 'OK':
+    PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+    settings.configure(
+        TEMPLATE_DIRS=(os.path.join(PROJECT_DIR, 'templates'),),
+    )
+    t = loader.get_template('issue.html')
+
     for num in messages[0].split(' '):
         if num:
             typ, data = conn.fetch(num,'(RFC822)')
             msg = email.message_from_string(data[0][1])
 
-            from_ = msg.get('From')
-            date = msg.get('Date')
-            subject = msg.get('Subject')
-            body = msg.get_payload()
-
             ret, data = conn.store(num,'+FLAGS', '\\Seen')
 
             if ret == 'OK':
-                print(from_)
-                print(date)
-                print(subject)
-                print(body)
+                c = Context({
+                    'from': msg.get('From'),
+                    'date': msg.get('Date'),
+                    'subject': msg.get('Subject'),
+                    'body':msg.get_payload()
+                })
+
+                print(t.render(c))
+
+                # If needed we can save this html to file
+                #import codecs
+                #f = codecs.open('/tmp/foobar.sla', "w", encoding="utf-8")
+                #f.write(t.render(c))
+                #f.close()
 
 conn.close()
