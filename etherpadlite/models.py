9afd0842ce910d78e9125b71ee52618efa18ae86
Eric Schrijver <eric@ericschrijver.nl>
2013-09-04 09:36:35 +0200

    Explain symbols for namespacing in admin

--- etherpadlite/models.py
+++ etherpadlite/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.contrib.auth.models import User, Group
@@ -143,8 +145,8 @@ class Pad(models.Model):
     """Schema and methods for etherpad-lite pads
     """
     name = models.CharField(max_length=256)
-    display_name = models.CharField(max_length=256, blank=True, verbose_name="Name as used in Display")
-    display_slug = models.CharField(max_length=256, blank=True, verbose_name="Name as used in URL")
+    display_name = models.CharField(max_length=256, blank=True, verbose_name=u"Name as used in Display (use → for namespacing)")
+    display_slug = models.CharField(max_length=256, blank=True, verbose_name="Name as used in URL (use :: for namespacing)")
     server = models.ForeignKey(PadServer)
     group = models.ForeignKey(PadGroup)
 
