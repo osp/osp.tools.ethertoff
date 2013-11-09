85300aee029a50931bf8f5fbb7fea0b4f3d1feac
Eric Schrijver <eric@ericschrijver.nl>
2013-11-09 17:31:19 +0100

    Sorry, these were not the correct links.

--- relearn/static/js/relearn.js
+++ relearn/static/js/relearn.js
@@ -73,7 +73,7 @@ var renderSection = function(section) {
         return '<li><a class="namespace">' + sectionKey + ' →</a>' + renderSection(section.sections[sectionKey]) + '</li>';
     }));
     var lis = lis.concat(_.map(section.pads, function(pad) {
-        return '<li><a href="' + pad.slug + '">' + pad.name + '</a></li>';
+        return '<li><a href="' + pad.href + '">' + pad.name + '</a></li>';
     }));
     return '<ul>' + lis.join('') + '</ul>';
 };
