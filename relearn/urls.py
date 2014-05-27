924a5c6fc1e51716babd6e3bc13eb237a05add51
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-05-27 11:44:46 +0200

    import httpresponse for robots.txt

--- relearn/urls.py
+++ relearn/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 from django.conf import settings
 from django.conf.urls.static import static
