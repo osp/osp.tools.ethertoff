4dcebb282930d7b497d1e09e25df5fb85ac45c8e
Eric Schrijver <eric@ericschrijver.nl>
2014-07-02 08:18:20 +0200

    No piwik when running locally
    With a context processor that introduces the LOCAL keyword

--- relearn/settings.py
+++ relearn/settings.py
@@ -117,6 +117,7 @@ TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
     "relearn.context_processors.site_name",
     "relearn.context_processors.pads",
     "relearn.context_processors.eye",
+    "relearn.context_processors.local",
 )
 
 INSTALLED_APPS = (
