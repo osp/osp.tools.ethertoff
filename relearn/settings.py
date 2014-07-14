f323e48cb94153c0d2989c3523b711e6df7a691b
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-07-14 12:28:49 +0200

    rm eye bis

--- relearn/settings.py
+++ relearn/settings.py
@@ -116,7 +116,6 @@ TEMPLATE_DIRS = (
 TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
     "relearn.context_processors.site_name",
     "relearn.context_processors.pads",
-    "relearn.context_processors.eye",
     "relearn.context_processors.local",
 )
 
