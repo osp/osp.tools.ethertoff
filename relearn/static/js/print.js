968be9d4fa7985ef5578e56cea1f396f10b2396b
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-08-19 16:01:39 +0200

    Le polyfill marche, avec quelques bugs encore.

--- relearn/static/js/print.js
+++ relearn/static/js/print.js
@@ -1,8 +1,8 @@
-//$(".page").css("height", "auto");
+console.log("ok");
 
 //// PRINT PREVIEW
 $(window).load(function(){
-    $("body").addClass("print-mode");
+    if($("body").hasClass("print-mode")){
     $("style[media='print']").attr("media", "print, screen");
     $("link[media='print']").attr("media", "print, screen");
 
@@ -16,14 +16,19 @@ $(window).load(function(){
     console.log("nb page = " + nb_page);
     //if (nb_page == 0) { nb_page = 1}
     for (i = 1; i <= nb_page; i++){
-        $("#master-page").clone().addClass("page").attr("id","page-"+i).insertAfter($("#master-page"));
+        $("#master-page").clone().addClass("page").attr("id","page-"+i).insertBefore($("#master-page"));
     }
     $("#master-page").hide();
 
+    
+
+            window.setTimeout(function(){
+                console.log("do it");
+                    CSSRegions.doLayout();
+            }, 5000);
 
 
 
-    //    if(! $("html").hasClass("print-mode")){
     //$(".page").height(page_height);
     //CSSRegions.doLayout();
     //        $("html").addClass("print-preview");
@@ -33,6 +38,8 @@ $(window).load(function(){
     //        $(html).removeClass("print-preview");
     //        $("style[media='print, screen']").attr("media", "print");
     //        $("link[media='print, screen']").attr("media", "print");
-    //    }
+    
+
+   }
 
 });
