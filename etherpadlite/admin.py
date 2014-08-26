6eec18dc412adfdb1e06e33af5e66648608a4e07
Eric Schrijver <eric@ericschrijver.nl>
2014-08-26 10:45:11 +0200

    Disable the add function through the admin
    It was not working correctly, so better have one way to do it (front-end) and to make sure it works

--- etherpadlite/admin.py
+++ etherpadlite/admin.py
@@ -12,6 +12,9 @@ class PadAdmin(admin.ModelAdmin):
     list_display = ('__unicode__',)
     readonly_fields = ('name',)
     exclude = ('display_name',)
+    
+    def has_add_permission(self, request):
+        return False
 
 admin.site.register(PadServer)
 admin.site.register(PadGroup)
