d9dd644df237efa9ccb363ed403ab31a6690c40d
Eric Schrijver <eric@ericschrijver.nl>
2014-08-28 12:13:18 +0200

    Clean up some unused views and routes
    Also determine group automatically in pad create view
    
    Remove comments that are out of date

--- relearn/views.py
+++ relearn/views.py
@@ -34,13 +34,7 @@ from etherpadlite.models import *
 from etherpadlite import forms
 from etherpadlite import config
 
-from django.shortcuts import render
-from django.core.mail import send_mail
-from relearn.forms import ContactForm
-
 from relearn.management.commands.index import snif
-
-from relearn.templatetags.wikify import dewikify
 
 # By default, the homepage is the pad called ‘start’ (props to DokuWiki!)
 try:
@@ -66,26 +60,16 @@ Create a regex for our include template tag
 """
 include_regex = re.compile("{%\s?include\s?\"([\w._-]+)\"\s?%}")
 
-
 @login_required(login_url='/accounts/login')
-def padCreate(request, pk):
-    """Create a named pad for the given group
-    So this is kind of convoluted. With an input like
-
-    Relearn::Can it scale to the universe
-
-    we get a pad, with the unchangeable id:
-    relearn::can-it-scale-to-the-universe
-    and the name / display_slug:
-    Relearn::Can_it_scale_to_the_universe
-
-    The title of pages is displayed through a slight transformation,
-    known as dewikify.
-    _ becomes space, :: → as in:
-    Relearn → can it scale to the universe
+def padCreate(request):
     """
-    group = get_object_or_404(PadGroup, pk=pk)
-
+    Create a pad
+    """
+    
+    # normally the ‘pads’ context processor should have made sure that these objects exist:
+    author = PadAuthor.objects.get(user=request.user)
+    group = author.group.all()[0]
+    
     if request.method == 'POST':  # Process the form
         form = forms.PadCreate(request.POST)
         if form.is_valid():
@@ -105,112 +89,13 @@ def padCreate(request, pk):
 
     con = {
         'form': form,
-        'pk': pk,
+        'pk': group.pk,
         'title': _('Create pad in %(grp)s') % {'grp': group.__unicode__()}
     }
     con.update(csrf(request))
     return render_to_response(
         'pad-create.html',
         con,
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required(login_url='/accounts/login')
-def padDelete(request, pk):
-    """Delete a given pad
-    """
-    pad = get_object_or_404(Pad, pk=pk)
-
-    # Any form submissions will send us back to the profile
-    if request.method == 'POST':
-        if 'confirm' in request.POST:
-            pad.delete()
-        return HttpResponseRedirect(reverse('profile'))
-
-    con = {
-        'action': '/etherpad/delete/' + pk + '/',
-        'question': _('Really delete this pad?'),
-        'title': _('Deleting %(pad)s') % {'pad': pad.__unicode__()}
-    }
-    con.update(csrf(request))
-    return render_to_response(
-        'etherpad-lite/confirm.html',
-        con,
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required(login_url='/accounts/login')
-def groupCreate(request):
-    """ Create a new Group
-    """
-    message = ""
-    if request.method == 'POST':  # Process the form
-        form = forms.GroupCreate(request.POST)
-        if form.is_valid():
-            group = form.save()
-            # temporarily it is not nessessary to specify a server, so we take
-            # the first one we get.
-            server = PadServer.objects.all()[0]
-            pad_group = PadGroup(group=group, server=server)
-            pad_group.save()
-            request.user.groups.add(group)
-            return HttpResponseRedirect(reverse('profile'))
-        else:
-            message = _("This Groupname is allready in use or invalid.")
-    else:  # No form to process so create a fresh one
-        form = forms.GroupCreate()
-    con = {
-        'form': form,
-        'title': _('Create a new Group'),
-        'message': message,
-    }
-    con.update(csrf(request))
-    return render_to_response(
-        'etherpad-lite/groupCreate.html',
-        con,
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required(login_url='/accounts/login')
-def groupDelete(request, pk):
-    """
-    """
-    pass
-
-
-@login_required(login_url='/accounts/login')
-def profile(request):
-    """Display a user profile containing etherpad groups and associated pads
-    """
-    name = request.user.__unicode__()
-
-    try:  # Retrieve the corresponding padauthor object
-        author = PadAuthor.objects.get(user=request.user)
-    except PadAuthor.DoesNotExist:  # None exists, so create one
-        author = PadAuthor(
-            user=request.user,
-            server=PadServer.objects.get(id=1)
-        )
-        author.save()
-    author.GroupSynch()
-
-    groups = {}
-    for g in author.group.all():
-        groups[g.__unicode__()] = {
-            'group': g,
-            'pads': Pad.objects.filter(group=g)
-        }
-
-    return render_to_response(
-        'etherpad-lite/profile.html',
-        {
-            'name': name,
-            'author': author,
-            'groups': groups
-        },
         context_instance=RequestContext(request)
     )
 
