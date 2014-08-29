8e47fd6f56935ffeaea9ea0d71b0d5616c83e23d
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-08-30 00:51:14 +0200

    On peut overrider le format de page, ainsi que le nombre de pages depuis le pad comme ceci:     <script>         var customPageWidth = "400mm";         var customPageHeight = "310mm";         var crop = "12mm";         var customPageNumber = 16;     </script>

--- relearn/static/js/print.js
+++ relearn/static/js/print.js
@@ -5,6 +5,34 @@ if($("body").hasClass("print-mode")){
     $("style[media='print']").attr("media", "print, screen");
     $("link[media='print']").attr("media", "print, screen");
 
+
+    // OVERRIDES PAGE FORMAT SET ON PAD
+    try{
+        var pageWidthCrop = parseInt(customPageWidth) + parseInt(crop) + "mm";
+        var pageHeightCrop = parseInt(customPageHeight) + parseInt(crop) + "mm";
+        var styles = "                                                  \
+            @page {                                                     \n \
+                size:" + pageWidthCrop + " " + pageHeightCrop + ";      \n \
+            }                                                           \n \
+            html,                                                       \n \
+            body {                                                      \n \
+                width: " + pageWidthCrop + ";                           \n \
+            }                                                           \n \
+            #master-page,                                               \n \
+            .page,                                                      \n \
+            div.print-marks {                                           \n \
+                width: " + pageWidthCrop + ";                           \n \
+                height: " + pageHeightCrop + ";                         \n \
+            }                                                           \n \
+        "
+        var style = document.createElement('style');
+        document.body.appendChild(style);
+        style.id = "publish-css";
+        style.media = "screen, print";
+        $("#publish-css").html(styles);
+    } catch(e){}
+
+
     // Create pages
     $("body").append('<section id="master-page" class="page"></section>');
     $("#master-page").append('\
@@ -32,7 +60,11 @@ if($("body").hasClass("print-mode")){
 
     height = $($(".middle")[0]).height() + $($(".middle")[1]).height();
     page_height = $("#master-page").height();
-    nb_page = Math.ceil(height / page_height);
+    try {
+        nb_page = customPageNumber;
+    } catch(e) {
+        nb_page = Math.ceil(height / page_height);
+    }
     console.log("Content = " + height);
     console.log("page height = " + page_height);
     console.log("nb page = " + nb_page);
