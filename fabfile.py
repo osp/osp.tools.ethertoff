ec78dcbbb1644b4df60e8f40035b6af91a5a70b5
Eric Schrijver <eric@ericschrijver.nl>
2014-09-07 14:37:25 +0200

    Fabulous Kitchen Fab File

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,17 @@
 from fabric.api import run, cd, env, local, sudo
 
-env.hosts = ['bat@93.191.132.95']
-env.path = '/home/bat/apps/relearn.be'
+env.hosts = ['osp@osp.kitchen']
+env.path = '/home/osp/apps/vj14/ethertoff'
 
 def getdb():
     pass
 
 def deploy():
     with cd(env.path):
-        run('git pull origin etherbat')
-        run('/home/bat/venvs/relearn/bin/python manage.py collectstatic --noinput')
-        sudo('/home/bat/venvs/relearn/bin/supervisorctl restart relearn')
+        run('git pull origin vj14')
+        run('/home/osp/apps/venvs/vj14/bin/python manage.py collectstatic --noinput')
+        sudo('/usr/bin/supervisorctl restart vj14')
 
 def index():
     with cd(env.path):
-        run('/home/bat/venvs/relearn/bin/python manage.py index')
+        run('/home/osp/apps/venvs/vj14/bin/python manage.py index')
