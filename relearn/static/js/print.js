b9cbe731e3ee64814da7526e51d72026196e3989
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-11-25 11:17:22 +0100

    retour du marginalia

--- relearn/static/js/print.js
+++ relearn/static/js/print.js
@@ -91,7 +91,7 @@ if($("body").hasClass("p-mode")){
             </div>\
     ');
 
-    $("#master-page").append('<section class="header" data-title="'+ $("h2[property='dc:title']").text() + '"></section>').append('<section class="main-section"><div class="column"></div></section>').append('<section class="footer"></section>');
+    $("#master-page").append('<section class="header" data-title="'+ $("h2[property='dc:title']").text() + '"></section>').append('<section class="main-section"><div class="marginalia"></div><div class="column"></div></section>').append('<section class="footer"></section>');
 
     // OPTIONAL COLUMNS
     try {
